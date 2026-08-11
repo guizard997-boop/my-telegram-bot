cc31fa5fdf4d8d9aa8718515ff171bdd20536b13
guizard997-boop <guizard997@gmail.com>
2026-08-11 19:36:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -600,8 +600,9 @@ def calc_max_buy_price(quick_sell, expenses=None, required_profit=None):
     profit = REQUIRED_PROFIT_USD if required_profit is None else required_profit
     after_reserve = quick_sell * (1 - NEGOTIATION_RESERVE)
     max_buy = after_reserve - exp - profit
-    if quick_sell > 0 and (quick_sell - max_buy) / quick_sell* (1 - MIN_PROFIT_RATIO) - exp
-    return max(0, round(max_buy))
+    if quick_sell > 0 and (quick_sell - max_buy) / quick_sell < MIN_PROFIT_RATIO:
+    max_buy = quick_sell * (1 - MIN_PROFIT_RATIO) - exp
+return max(0, round(max_buy))
 
 
 def find_comparables(target_specs):
